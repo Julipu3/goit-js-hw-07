b5332388a7115eccbdd3bb3d42fd55b769a096c7
Julipu3 <julia.pim55@gmail.com>
2023-08-03 14:42:05 +0300

    changed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,38 +3,31 @@ import { galleryItems } from './gallery-items.js';
  
 const gallery = document.querySelector(".gallery");
 
-// gallery.addEventListener("click", handleClick);
 
 function createMarkup() {
 
     const element = galleryItems.map(item => `
 
 <li class="gallery__item">
-   <a class="gallery__link" href="large-image.jpg">
+   <a class="gallery__link" href="${item.original}">
       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
    </a>
 </li>
-    `).join("");
+    `).join(""); 
 
     gallery.innerHTML = element;
 };
 
 createMarkup()
 
-gallery.addEventListener("click", modalClick);
-function modalClick(e) {
-e.preventDefault();
-const target = e.target;
-if (target === e.currentTarget) {
-    return;
-}
-modalSlider();
-}
 
 const lightbox = new SimpleLightbox(".gallery a", {
     captionsData: 'alt',
     captionDelay: 250,
 });
 
-
 console.log(galleryItems);
+
+
+
+
